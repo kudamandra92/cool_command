4d4f51c80059291daaccdf6d6444b735da1d6707
kudamandra <kudamandra@gmail.com>
2025-10-22 19:25:46 +0800

    give mobile view login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginForm from "./LoginForm";
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-limeish/60">
-      <div className=" p-8 rounded-xl w-[20vw]">
+      <div className=" p-8 rounded-xl w-full lg:w-[20vw]">
         <h1 className="text-3xl font-bold mb-6 text-center font-sans">
           Login
         </h1>
